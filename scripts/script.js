74d21de6c5fcf604e7b62984485b55a8562424e6
romz_ru <romz_ru@mail.ru>
2016-02-22 01:22:51 +0200

    add paginator-3000

--- scripts/script.js
+++ scripts/script.js
@@ -4,15 +4,15 @@ $(document).ready(function(){
 	});
  // создание массива тегов
             var word_list = [
-                {text:"Lorem", weight:13, link:"https://github.com/DukeLeNoir/jQCloud"},
-                {text:"Ipsum", weight:10.5, html:{title:"My Title", "class":"custom-class"}, link:{href:"http://jquery.com/", target:"_blank"}},
-                {text:"Dolor", weight:9.4},
-                {text:"Sit", weight:8},
-                {text:"Amet", weight:6.2},
-                {text:"Consectetur", weight:5},
-                {text:"Adipiscing", weight:5},
-                {text:"Elit", weight:5},
-                {text:"Nam et", weight:5}
+                {text:"Родинки", weight:13, link:"https://github.com/DukeLeNoir/jQCloud"},
+                {text:"Родинки", weight:10.5, html:{title:"My Title", "class":"custom-class"}, link:{href:"http://jquery.com/", target:"_blank"}},
+                {text:"Родинки", weight:9.4},
+                {text:"Родинки", weight:8},
+                {text:"Родинки", weight:6.2},
+                {text:"Родинки", weight:5},
+                {text:"Родинки", weight:5},
+                {text:"Родинки", weight:5},
+                {text:"Родинки", weight:5}
             ];
             $(function () {
                 $("#tags").jQCloud(word_list);
