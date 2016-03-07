c4cd6d9680048fec1fd8b1aad1ce0a04dbbc2ae7
morontt <morontt@gmail.com>
2016-03-07 21:28:09 +0200

    reformat code

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,40 @@
-$(document).ready(function(){
-	$(function(){
-	alert('Подключена последняя версия jQuery через Google хостинг');
-	});
- // создание массива тегов
-            var word_list = [
-                {text:"родинки", weight:13, link:{href:"#post1"}},
-                {text:"узоры", weight:10.5, link:{href:"#post2"}},
-                {text:"линия сердца", weight:9.4},
-                {text:"род", weight:8},
-                {text:"родин", weight:6.2},
-                {text:"родинк", weight:5},
-                {text:"родин", weight:5},
-                {text:"родин", weight:5},
-                {text:"родин", weight:5}
-            ];
-            $(function () {
-                $("#tags").jQCloud(word_list);
-            });
-          //  alert( $("#blog_page").css('height'));
-          //  alert(getComputedStyle(blog_page).height);
-          // $('#tags').css({height: $("#blog_page").css('height')});
-             $('#tags').css({height: '1671px'});
+$(document).ready(function () {
+    // создание массива тегов
+    var word_list = [
+        {text: "родинки", weight: 13, link: {href: "#post1"}},
+        {text: "узоры", weight: 10.5, link: {href: "#post2"}},
+        {text: "линия сердца", weight: 9.4},
+        {text: "род", weight: 8},
+        {text: "родин", weight: 6.2},
+        {text: "родинк", weight: 5},
+        {text: "родин", weight: 5},
+        {text: "родин", weight: 5},
+        {text: "родин", weight: 5}
+    ];
+    $(function () {
+        $("#tags").jQCloud(word_list);
+    });
+    //  alert( $("#blog_page").css('height'));
+    //  alert(getComputedStyle(blog_page).height);
+    // $('#tags').css({height: $("#blog_page").css('height')});
+    $('#tags').css({height: '1671px'});
 
-			
-	$(".entry_content").hide();
-	$('<span class="readmore_collapse">Read more..</span>').insertAfter(".first_p");
-	 
-	  $(".readmore_collapse").hover(function(){
-	     $(this).addClass('readHover')},
-	     function(){
-		$(this).removeClass('readHover');  
-	  });
-	 $('.readmore_collapse').click(function(){
-	 $(this).next().toggle();
-	 if($(this).next().is(':visible')){
-		$(this).text('Collapse')	 
-	 }else{$(this).text('Read more..')}
-	 });
-	 $('#tags') 
-	      	
-	
+    $(".entry_content").hide();
+    $('<span class="readmore_collapse">Read more..</span>').insertAfter(".first_p");
+
+    $(".readmore_collapse").hover(function () {
+            $(this).addClass('readHover')
+        },
+        function () {
+            $(this).removeClass('readHover');
+        });
+    $('.readmore_collapse').click(function () {
+        $(this).next().toggle();
+        if ($(this).next().is(':visible')) {
+            $(this).text('Collapse')
+        } else {
+            $(this).text('Read more..')
+        }
+    });
+    $('#tags')
 });
