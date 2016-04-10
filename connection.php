9bca01b788a5ab71cd43ffdcd907df8a81f5b56d
romz_ru <romz_ru@mail.ru>
2016-04-10 21:20:21 +0300

    add connection.php

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+
+  $connection = mysql_connect("localhost", "root", "root");
+  $db = mysql_select_db("bfl_blog");
+  mysql_set_charset("utf8");	
+	if(!$connection || !$db){
+	
+	  exit(mysql_error());
+	}
+ 
+  ?>
