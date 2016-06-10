f583232f7f79de07560c07e00a97569dac3666e1
romz_ru <romz_ru@mail.ru>
2016-06-10 18:01:57 +0300

    customization

--- src/app.php
+++ src/app.php
@@ -1,7 +1,6 @@
 <?php
 
 use Silex\Application;
-use Silex\Provider\AssetServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\HttpFragmentServiceProvider;
@@ -12,7 +11,6 @@ $app->register(new AssetServiceProvider());
 $app->register(new TwigServiceProvider());
 $app->register(new HttpFragmentServiceProvider());
 $app['twig'] = $app->extend('twig', function ($twig, $app) {
-    // add custom globals, filters, tags, ...
 
     return $twig;
 });
