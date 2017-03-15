7853757e0ee7fad1eed0133520980ebe5470e087
romz_ru <romz_ru@mail.ru>
2017-03-15 23:14:27 +0200

    change index.html

--- web/index.php
+++ web/index.php
@@ -1,42 +1,17 @@
 <?php
 
-// web/index.php
-require_once __DIR__.'/../vendor/autoload.php';
+	require_once __DIR__.'/../vendor/autoload.php';
 
-$app = new Silex\Application();
+	$app = new Silex\Application();
 
-// $app->get('/hello/{name}', function ($name) use ($app) {
-//     return 'Hello '.$app->escape($name);
-// });
+	$app -> get('/hello/{name}', function($name) use($app){
 
+	return "Hello ".$app -> escape($name);	
 
+	});
 
-$blogPosts = array(
-    1 => array(
-        'date'      => '2011-03-29',
-        'author'    => 'igorw',
-        'title'     => 'Using Silex',
-        'body'      => '...',
-    ),
-);
+	$app->run();
 
 
 
-
-
-
-
-$app->get('/blog/{id}', function (Silex\Application $app, $id) use ($blogPosts) {
-    if (!isset($blogPosts[$id])) {
-        $app->abort(404, "Post $id does not exist.");
-    }
-
-    $post = $blogPosts[$id];
-
-    return  "<h1>{$post['title']}</h1>".
-            "<p>{$post['body']}</p>";
-});
-
-
-
-$app->run();
+?>
